c662d62defdeff36cfe4fcf7eba4fd23f142aee6
Cristian Pereyra <cristian@redmintlabs.com>
2013-08-01 14:34:43 -0300

    adds tests of home controller

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,3 +1,19 @@
 module Helpers
+	def sign_in_with username, password
+		visit root_path
+		within("#new_user") do
+		  fill_in 'user[email]', :with => username
+		  fill_in 'user[password]', :with => password
+		end
+		click_button('Sign in')
+		visit root_path
+	end
 
+	def sign_in_example_user!
+		sign_in_with 'user@example.com', 'testpass1'
+	end
+
+	def create_example_user!
+		User.create!(:email => 'user@example.com', :password => 'testpass1')
+	end
 end
